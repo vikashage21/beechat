5b97e1fb2c102cd6ec007ac398371bc27c8dd993
vikash sharma <vk220783@gmail.com>
2023-11-16 12:31:31 +0530

    fixing some error

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE).then(()=>{
+mongoose.set('strictQuery', false); // Address the deprecation warning
+
+mongoose.connect(process.env.DATABASE,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log('connecting to database succesfully')
 }).catch((e)=>{
 
